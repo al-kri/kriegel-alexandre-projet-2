edc4224cee368681824a214831ee1f73b732ba0a
Alexandre Kriegel <sr36vkcg84k7-student@anonymous-codio.com>
2023-03-05 13:16:13 +0000

    final version with results.out file

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -12,7 +12,7 @@ public class Main {
   */
   public static void main(String[] args) {
     ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
-    ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+    ISymptomWriter writer = new WriteSymptomDataToFile("results.out");
     AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
     List<String> symptoms = analyticsCounter.getSymptoms();
     Map<String, Integer> countSymptoms = analyticsCounter.countSymptoms(symptoms);
